66b69d54c9d832e49405cdac3e65a8686d4d358b
Siddharth <siddujain525@gmail.com>
2026-06-18 14:41:22 +0530

    Create binarysearch.java

---
+++ Array/binarysearch.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        for (int i = 0 ; i < nums.length; i ++){
+            if(nums[i] == target ){
+                return i;
+            }  
+        }
+        return -1;
+    }
+}
+
+*/currently solved using brute force will implement binay search soon */
+  
