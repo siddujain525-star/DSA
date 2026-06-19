f40a79299c93f3543a0e1ed2a1698b0fab88108c
Siddharth <siddujain525@gmail.com>
2026-06-19 11:38:53 +0530

    Create movezeros.java

---
+++ Array/movezeros.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int zeroCount = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] == 0 ) {
+                zeroCount++;
+            }
+            else if (zeroCount > 0) {
+                nums[i - zeroCount] = nums[i];
+                nums[i] = 0;
+            }
+        }
+        
+    }
+}
+*/ shifting the indexs*/
