fb5203ebaa32cd376da56d3b2197878547e94e0d
Siddharth <siddujain525@gmail.com>
2026-06-19 12:03:13 +0530

    Create highestaltitude.java

---
+++ Array/highestaltitude.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int current = 0 ;
+        int highest = 0 ;
+        for(int i = 0 ; i < gain.length ; i++){
+            current += gain[i];
+            highest = Math.max(current, highest);
+        }
+        return highest;
+    }
+}
