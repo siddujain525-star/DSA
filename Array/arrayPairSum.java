3e9abbd456fe9693d3f428a6a3f20609b679c630
Siddharth <siddujain525@gmail.com>
2026-09-05 22:21:10 +0530

    Create arrayPairSum.java

---
+++ Array/arrayPairSum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int sum = 0 ;
+        for(int i =0 ; i < n  ; i=i+2){
+            sum = sum+nums[i];
+        }
+        return sum;
+        
+    }
+}
